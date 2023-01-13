766ec94522958dcb591ec353572d028e13f67b83
leonardo.carvalho <leo.carvalholkp@gmail.com>
2023-01-12 23:06:15 -0300

    feat: create find by email

--- src/app/repositories/user-repository.ts
+++ src/app/repositories/user-repository.ts
@@ -2,4 +2,5 @@ import { userStoreType } from "@/domain/user/dtos"
 
 export interface UserRepository {
   signIn(data: userStoreType): Promise<{ id: string }>
+  findByEmail(email: string): Promise<userStoreType | null>
 }
