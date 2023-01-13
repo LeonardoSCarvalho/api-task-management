e7a39b478bccb1d2a007b5b54def257afe141c19
leonardo.carvalho <leo.carvalholkp@gmail.com>
2023-01-12 23:06:41 -0300

    feat: create find by email in memory

--- src/infra/repositories/in-memory-repositories/in-memory-user-repository.ts
+++ src/infra/repositories/in-memory-repositories/in-memory-user-repository.ts
@@ -4,9 +4,15 @@ import { userStoreType } from "@/domain/user/dtos"
 export class InMemoryUserRepository implements UserRepository {
   private readonly users: userStoreType[] = []
   public signInCalls = 0
+  public findByEmailCalls = 0
   async signIn(data: userStoreType): Promise<{ id: string }> {
     this.signInCalls++
     this.users.push(data)
     return { id: this.users.at(-1)?.id || "" }
+  }
+  async findByEmail(email: string): Promise<userStoreType | null> {
+    this.findByEmailCalls++
+    const user = this.users.find((user) => user.email === email)
+    return user || null
   }
 }
